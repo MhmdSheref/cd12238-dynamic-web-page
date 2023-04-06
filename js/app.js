6891f21a8457c9e38ebeac80453a3b8085872222
MhmdSheref <88167888+MhmdSheref@users.noreply.github.com>
2023-04-06 12:45:09 +0200

    I did the whole project in one go basically

--- js/app.js
+++ js/app.js
@@ -1,7 +1,78 @@
-/** TODO: Build the navigation bar using JavaScript **/
+let itemList = document.querySelectorAll("section");
+let commentForm = document.querySelector(".comment__form");
+let commentView = document.querySelector(".comments__container");
 
-/** TODO: Add smooth scrolling **/
+let Navbar = {
 
-/** TODO: Add an active state **/
+    navbarDOM: document.querySelector("#navbar__list"),
 
-/** TODO: Add a comment form **/
+    makeNavBar: function () {
+        itemList.forEach((ele) => {
+            let anchor = document.createElement("a");
+            let listItem = document.createElement("li");
+            if (ele.dataset["nav"]) anchor.innerText = ele.dataset["nav"];
+            else anchor.innerText = ele.querySelector("h2").innerText;
+
+            anchor.setAttribute("href", "#" + ele.id);
+            anchor.classList.add("menu__link");
+            listItem.append(anchor);
+            this.navbarDOM.append(listItem);
+        })
+        },
+
+    scroller: function (event) {
+        let target = event.target
+        if (!target.classList.contains("menu__link")) return;
+        event.preventDefault();
+        document.querySelector(target.getAttribute("href")).scrollIntoView({behavior: "smooth"});
+    }
+}
+
+function setActive() {
+    for (let i = 0; i < itemList.length; i++) {
+        if (itemList[i].getBoundingClientRect().top + itemList[i].getBoundingClientRect().height/2 > 0) {
+            itemList[i].classList.add("active");
+        }else {
+            itemList[i].classList.remove("active");
+        }
+    }
+}
+
+function appendComment(name, email, comment) {
+    let outer = document.createElement("div");
+    let inner = `<h4 class="name__view">${name}</h4>
+                 <p class="email__view">${email}</p>
+                 <p>${comment}</p>`;
+    outer.classList.add("comment");
+    outer.innerHTML = inner;
+
+    commentView.append(outer)
+}
+function verifyForm(e) {
+    e.preventDefault()
+    let name = commentForm.querySelector("#name").value;
+    let email = commentForm.querySelector("#email").value;
+    let comment = commentForm.querySelector("#comment__text").value;
+    if (!name) {
+        alert("You need to input a name");
+        return;
+    }
+    if ((!email) || (!email.includes("@"))) {
+        alert("You need to input a valid email")
+        return;
+    }
+    if (!comment) {
+        alert("You need to input a comment")
+        return;
+    }
+    appendComment(name, email, comment)
+}
+
+
+function main() {
+    Navbar.makeNavBar();
+    Navbar.navbarDOM.addEventListener("click", Navbar.scroller);
+    document.addEventListener("scroll", setActive);
+    commentForm.querySelector("#submit").addEventListener("click", verifyForm)
+}
+main();
