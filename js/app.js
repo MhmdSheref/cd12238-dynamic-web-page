b07ea2d8fa01e4df3f9370e9644ed3350abb9013
MhmdSheref <88167888+MhmdSheref@users.noreply.github.com>
2023-04-06 19:50:50 +0200

    comments now save to localstorage as well as grouping comment in its object

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 let itemList = document.querySelectorAll("section");
-let commentForm = document.querySelector(".comment__form");
-let commentView = document.querySelector(".comments__container");
 
 let Navbar = {
 
@@ -37,45 +35,68 @@ function setActive() {
         }
     }
 }
+let CommentHandler = {
+    commentForm: document.querySelector(".comment__form"),
+    commentView: document.querySelector(".comments__container"),
+    commentList: [],
 
-function appendComment(name, email, comment) {
-    let outer = document.createElement("div");
-    let inner = `<h4 class="name__view">${name}</h4>
-                 <p class="email__view">${email}</p>
-                 <p>${comment}</p>`;
-    outer.classList.add("comment");
-    outer.innerHTML = inner;
+    appendComment: function (name, email, comment) {
+        let outer = document.createElement("div");
+        let inner = `<h4 class="name__view">${name}</h4>
+                     <p class="email__view">${email}</p>
+                     <p>${comment}</p>`;
+        outer.classList.add("comment");
+        outer.innerHTML = inner;
 
-    commentView.append(outer)
+        this.commentView.append(outer)
+    },
+
+    verifyForm: function (e) {
+        e.preventDefault()
+        let name = this.commentForm.querySelector("#name").value;
+        let email = this.commentForm.querySelector("#email").value;
+        let comment = this.commentForm.querySelector("#comment__text").value;
+        if (!name) {
+            alert("You need to input a name");
+            return;
+        }
+        if ((!email) || (!email.includes("@"))) {
+            alert("You need to input a valid email");
+            return;
+        }
+        if (!comment) {
+            alert("You need to input a comment");
+            return;
+        }
+        let comData = [name, email, comment];
+        this.saveToLocal(comData)
+        this.appendComment(...comData);
+        this.commentForm.querySelector("#name").value = "";
+        this.commentForm.querySelector("#email").value = "";
+        this.commentForm.querySelector("#comment__text").value = "";
+    },
+
+    saveToLocal: function (comment) {
+        this.commentList.push(comment);
+        localStorage.setItem("comments", JSON.stringify(this.commentList));
+    },
+
+    loadLocal: function () {
+        if (!localStorage.getItem("comments")) return;
+        this.commentList = JSON.parse(localStorage.getItem("comments"));
+        this.commentList.forEach((item)=>{this.appendComment(...item)});
+    },
 }
-function verifyForm(e) {
-    e.preventDefault()
-    let name = commentForm.querySelector("#name").value;
-    let email = commentForm.querySelector("#email").value;
-    let comment = commentForm.querySelector("#comment__text").value;
-    if (!name) {
-        alert("You need to input a name");
-        return;
-    }
-    if ((!email) || (!email.includes("@"))) {
-        alert("You need to input a valid email")
-        return;
-    }
-    if (!comment) {
-        alert("You need to input a comment")
-        return;
-    }
-    appendComment(name, email, comment)
-    commentForm.querySelector("#name").value = "";
-    commentForm.querySelector("#email").value = "";
-    commentForm.querySelector("#comment__text").value = "";
-}
+// let commentForm = document.querySelector(".comment__form");
+// let commentView = document.querySelector(".comments__container");
+// let commentList = [];
 
 
 function main() {
     Navbar.makeNavBar();
     Navbar.navbarDOM.addEventListener("click", Navbar.scroller);
     document.addEventListener("scroll", setActive);
-    commentForm.querySelector("#submit").addEventListener("click", verifyForm)
+    CommentHandler.commentForm.querySelector("#submit").addEventListener("click", CommentHandler.verifyForm.bind(CommentHandler));
+    CommentHandler.loadLocal()
 }
 main();
